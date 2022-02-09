1fc76cb4b461142b3616eecb096cf2f0515ed3be
GregSym <gtsrex@gmail.com>
2022-02-09 22:41:11 +0200

    baby's first grid

--- src/scripts/grid.jsx
+++ src/scripts/grid.jsx
@@ -1,23 +1,34 @@
 import { Component } from "react";
+import { rowStyle } from "../styles/grid.css";
 
-const mockGrid = [[1, 0, 0],
-[0, 0, 0],
-[0, 0, 1]]
-
+const mockGrid = [
+  [1, 0, 0],
+  [0, 0, 0],
+  [0, 0, 1],
+];
 
 class Cell extends Component {
-    render() {
-        return <div>
-            <h1>Underlying number: {this.props.value}</h1>
-        </div>
-    }
+  render() {
+    return (
+      <div>
+        <h1>Underlying value: {this.props.value}</h1>
+      </div>
+    );
+  }
 }
 
 export default class Grid extends Component {
-
-    render() {
-        return <div>
-            {mockGrid.map(row => <span>{row.map(value => <Cell value={value} />)}</span>)}
-        </div>
-    }
+  render() {
+    return (
+      <div>
+        {mockGrid.map((row) => (
+          <div className="rowStyle" float="left">
+            {row.map((value) => (
+              <Cell value={value} />
+            ))}
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
